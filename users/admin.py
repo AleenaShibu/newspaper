9520df087a9d75dc10697c1c4a1d8abe0c2842c1
Aleena Shibu <aleenashibu94@gmail.com>
2019-09-18 16:12:19 +0530

    created login logout and signup pages

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+	add_form= CustomUserCreationForm
+	form = CustomUserChangeForm
+	model = CustomUser
+	list_display = ['email','username','age','is_staff' ,]
+
+	fieldsets = (
+		(None,{'fields':('email','password','age')}),
+		('permissions',{'fields':('is_staff','is_active')}),
+		)
+	add_fieldsets = (
+		(None, {'classes':('wide',),
+			'fields':('email','password','password2','is_staff','is_active')
+			}),
+		)
+			
+admin.site.register(CustomUser,CustomUserAdmin)	
